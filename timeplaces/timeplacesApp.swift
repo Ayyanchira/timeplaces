17f6c120fb3cc84182fdb96dd4e2297e26608962
Akshay Ayyanchira <ayyanchira.akshay@gmail.com>
2025-02-11 06:31:36 -0800

    Initial Commit

---
+++ timeplaces/timeplacesApp.swift
@@ -0,0 +1,17 @@
+//
+//  timeplacesApp.swift
+//  timeplaces
+//
+//  Created by Akshay Ayyanchira on 2/11/25.
+//
+
+import SwiftUI
+
+@main
+struct timeplacesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ timeplacesTests/timeplacesTests.swift
@@ -0,0 +1,17 @@
+//
+//  timeplacesTests.swift
+//  timeplacesTests
+//
+//  Created by Akshay Ayyanchira on 2/11/25.
+//
+
+import Testing
+@testable import timeplaces
+
+struct timeplacesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
